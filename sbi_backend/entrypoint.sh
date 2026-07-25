c04919b23d0f8b9994a08c023d75c9bc34dd72e7
Khoase Hashatsi <Khoasehashatsi40@gmail.com>
2026-07-25 12:39:27 +0200

    aotadte

---
+++ sbi_backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Applying migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+
+gunicorn sbi_backend.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8000} \
+    --workers 3 \
+    --timeout 120
